af11878d9884772b197e28324fe7ebc5aca26ef2
gracegalloway <s2515636@ed.ac.uk>
2026-07-24 08:57:51 +0000

    update

--- Functions/functions.py
+++ Functions/functions.py
@@ -155,3 +155,220 @@ def init_depot(route):
     route = route[depot_pos:] + route[:depot_pos]
     route.append(0)
     return route
+
+def sa_split_CVRP():
+    n = len(coordinates)
+
+    # all customers no depot
+    nodes = list(range(1, n))
+
+    sub_dist = distance_matrix[np.ix_(nodes, nodes)]
+
+    # route first via sa
+    sol = solve_tsp(sub_dist, TIME=0.5*n, temperature=250, cooling=0.999)
+
+    big_route = [nodes[node] for node in sol["route"]]
+
+    routes = []
+    current_route = [0]      # start at depot
+    current_load = 0
+
+    # cluster by splitting 
+    for node in big_route:
+
+        if current_load + demands[node] <= vehicle_capacity:
+            current_route.append(node)
+            current_load += demands[node]
+
+        else:
+            # return to depot
+            current_route.append(0)
+            routes.append((current_route, current_load))
+
+            # new route
+            current_route = [0, node]
+            current_load = demands[node]
+
+    current_route.append(0)
+    routes.append((current_route, current_load))
+
+    return routes
+
+def sweep_sa_CVRP():
+    # cluster first
+    clusters = sweep_clusters(
+    coordinates,
+    demands,
+    vehicle_capacity
+)
+
+    for cluster in clusters:
+        assert sum(demands[i] for i in cluster) <= vehicle_capacity
+
+    routes = []
+
+    for cluster in clusters:
+
+        nodes = [0] + cluster
+
+        sub_dist = distance_matrix[np.ix_(nodes,nodes)]
+
+        # SA parameters based on cluster size
+        cluster_size = len(nodes)
+
+        temperature = 20 * cluster_size
+
+        if cluster_size <= 10:
+            cooling = 0.995
+        elif cluster_size <= 30:
+            cooling = 0.997
+        else:
+            cooling = 0.999
+
+        sol = solve_tsp(sub_dist, TIME=0.5*cluster_size, temperature=temperature, cooling=cooling)
+
+        route = [nodes[i] for i in sol["route"]]
+        route = init_depot(route)
+
+        load = sum(demands[node] for node in cluster)
+
+        routes.append((route, load))
+
+    return routes
+
+def sweep_clusters(coordinates, demands, vehicle_capacity):
+    depot = coordinates[0]
+    clusters = []
+    nodes = []
+
+    # Calculate angle of each node relative to depot
+    for node in range(1, len(coordinates)):
+
+        x, y = coordinates[node]
+
+        angle = math.atan2(
+            y - depot[1],
+            x - depot[0]
+        )
+
+        nodes.append((node, angle))
+
+
+    # Sort nodes by angle around depot
+    nodes.sort(key=lambda x: x[1])
+
+    # start at random node
+    start = random.randint(0, len(nodes)-1)
+    nodes = nodes[start:] + nodes[:start]
+
+    cluster = []
+    load = 0
+
+    # Sweep around depot
+    for node, angle in nodes:
+
+        demand = demands[node]
+
+        # If customer does not fit, get new vehicle
+        if load + demand > vehicle_capacity:
+
+            clusters.append(cluster)
+
+            cluster = []
+            load = 0
+
+
+        cluster.append(node)
+        load += demand
+
+    # Add final cluster
+    if cluster:
+        clusters.append(cluster)
+
+    return clusters
+
+def nn_sa_CVRP():
+    # cluster first
+    clusters = nearest_neighbour_clusters(
+    coordinates,
+    demands,
+    vehicle_capacity
+)
+
+    for cluster in clusters:
+        assert sum(demands[i] for i in cluster) <= vehicle_capacity
+
+    routes = []
+
+    for cluster in clusters:
+
+        nodes = [0] + cluster
+
+        sub_dist = distance_matrix[np.ix_(nodes,nodes)]
+
+        # SA parameters based on cluster size
+        cluster_size = len(nodes)
+
+        temperature = 20 * cluster_size
+
+        if cluster_size <= 10:
+            cooling = 0.995
+        elif cluster_size <= 30:
+            cooling = 0.997
+        else:
+            cooling = 0.999
+
+        sol = solve_tsp(sub_dist, TIME=0.5*cluster_size, temperature=temperature, cooling=cooling)
+
+        route = [nodes[i] for i in sol["route"]]
+        route = init_depot(route)
+
+        load = sum(demands[node] for node in cluster)
+
+        routes.append((route, load))
+
+    return routes
+
+def nearest_neighbour_clusters(coordinates, demands, vehicle_capacity):
+
+    nodes = set(range(1, len(coordinates)))  # no depot
+    clusters = []
+
+    while nodes:
+
+        cluster = []
+        load = 0
+        current_node = 0  # start from depot
+
+
+        while True:
+
+            feasible = [
+                node for node in nodes
+                if load + demands[node] <= vehicle_capacity
+            ]
+
+            if not feasible:
+                break
+
+
+            # choose nearest feasible node
+            next_node = min(
+                feasible,
+                key=lambda node: distance(current_node, node)
+            )
+
+
+            cluster.append(next_node)
+
+            load += demands[next_node]
+
+            nodes.remove(next_node)
+
+            current_node = next_node
+
+
+        clusters.append(cluster)
+
+
+    return clusters
